8a23733d284ee4a2400103403830eb19e3998733
Ibnu Hisyam Al-Lathif <hisyam.lateaf@gmail.com>
2017-04-05 09:25:16 +0700

    Update JavaApplication.java

--- JavaApplication.java
+++ JavaApplication.java
@@ -25,10 +25,10 @@ public class JavaApplication {
 
         for(i=2;i<count;++i)//loop starts from 2 because 0 and 1 are already printed    
         {    
-         n3=n1+n2;    
-         System.out.print(" "+n3);    
+         n3=n1+n2;
          n1=n2;    
          n2=n3;    
         }
+         System.out.print(n3);
     }
 }
